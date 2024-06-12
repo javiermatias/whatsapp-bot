4e4bfcca8368c2c7adb9b3da9161b958025d2f72
Javier Matias <javierjimenez78@gmail.com>
2024-06-12 01:41:31 -0300

    new inf

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const whatsappController = require("../controllers/whatsapp.controller");
 
-router.get("/whatsapp", whatsappController.verifyToken).post("/whatsapp", whatsappController.receiveMessage)
+router.get("/", whatsappController.verifyToken).post("/", whatsappController.receiveMessage)
 
 
 module.exports = router
