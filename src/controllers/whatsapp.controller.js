9e555c55f448c2470cc52caa8168bdac9597c894
Javier Matias <javierjimenez78@gmail.com>
2024-06-12 02:01:07 -0300

    add verify token

--- src/controllers/whatsapp.controller.js
+++ src/controllers/whatsapp.controller.js
@@ -1,5 +1,20 @@
 const verifyToken = (req, res) => {
-    res.send("Hola Mundo strange cabrones")
+
+    try {
+        let accessToken = "ASDA#$@RRDFS#@$DFSVXC234234SDF";
+        let token = req.query["hub.verify_token"];
+        let challenge = req.body["hub.challenge"];
+
+        if (challenge != null && token != null & token == accessToken) {
+            res.send(challenge);
+        } else {
+            res.status(400).send();
+        }
+
+    } catch (ex) {
+        res.status(400).send();
+    }
+
 }
 
 const receiveMessage = (req, res) => {
