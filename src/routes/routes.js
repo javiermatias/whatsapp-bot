538f19661e6f434963ebff4c934f0d475b16e3d8
Javier Matias <javierjimenez78@gmail.com>
2024-08-08 16:30:48 -0300

    add middleware

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const whatsappController = require("../controllers/whatsapp.controller");
 const middleWare = require("../middleware/middleware")
 
-router.get("/", whatsappController.verifyToken).post("/", whatsappController.receiveMessage)
+router.get("/", whatsappController.verifyToken).post("/", middleWare.carrefour, whatsappController.receiveMessage)
 
 router.get("/test", middleWare.carrefour, whatsappController.test)
 module.exports = router
