8a7176dabb0e6bbdbe852f1bbe6be8832e1b4b59
Javier Matias <javierjimenez78@gmail.com>
2024-08-26 21:06:38 -0300

    add routes

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,13 +1,21 @@
-const carrefour = (req, res, next) => {
-    let phone = req.body.entry[0].changes[0].value.metadata.display_phone_number;
-    console.log(phone);
-    req.empresa = { id:1079, nombre:"Carrefour"};
-    next();
+const companies = [{ id:1079, nombre:"Carrefour", number:"5493543609446"}]
+
+const company = (req, res, next) => {
+    try{
+        let phone = req.body.entry[0].changes[0].value.metadata.display_phone_number;
+        let company = companies.find(x => x.number === phone);        
+        if(company) { req.empresa = company; console.log(company);}        
+        next();      
+    }catch(e){    
+        Sentry.captureException(e);
+    }
+
+ 
 };
 
 
 
 
 module.exports = {
-    carrefour
+    company
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const whatsappController = require("../controllers/whatsapp.controller");
 const middleWare = require("../middleware/middleware")
 
-router.get("/", whatsappController.verifyToken).post("/", middleWare.carrefour, whatsappController.receiveMessage)
+router.get("/", whatsappController.verifyToken).post("/", middleWare.company, whatsappController.receiveMessage)
 
-router.get("/test", middleWare.carrefour, whatsappController.test)
+router.get("/test", middleWare.company, whatsappController.test)
 module.exports = router
