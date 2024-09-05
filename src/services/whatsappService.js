93bfec6975759e7362af8f95adc68a567e1df09c
Javier Matias <javierjimenez78@gmail.com>
2024-09-04 23:07:22 -0300

    add service

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -207,9 +207,9 @@ async function postIncidenciaNo(data, token) {
     const url = 'https://graph.facebook.com/v19.0/250745364796553/messages';
     const headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer EAAOYsaSAfZBYBOygJffraK4UnliD3swW9Goy8NSJ1JrqE69UJRTVqSnvi4U4ZClvy3SGgW2SrOTMEmLmShMY2xzyE4VQ1fRXQdDb7bAlti3VHbJo9SYgvuQyrfOQjDV8r0SwRLJPpZCtaMlLX7LAJxnCsBsJgwUNaS5o6TDSFcJATKFx53hYk0Ax3ZCn1Lwa"
+        "Authorization": "Bearer EAAOYsaSAfZBYBO57Y9B0n4ds9TghahxxcmUeptA19Px9J4Ul6k4YUg1h9Rw80VZApDQs1fIVZA2SmcKtvkLB2fYZAd6hFvHOneBhE0ZAk5SxLMsalRhRKnxwKe8KffyZBbRH480pLKZAjexioopIxZBs9GRKt5KOfb0POKudRexyKiIjoiZAyTkIYvSZBA0sN4XhHNcQZDZD"
     };
-
+    
     try {
         const response = await axios.post(url, data, { headers });
         //console.log(response.data);
@@ -234,29 +234,7 @@ async function loginToAusentismosOnline(username, password) {
   }
 }
 
-/* function sendMessage(data) {
 
-    const options = {
-        host: "graph.facebook.com",
-        path: "/v19.0/250745364796553/messages",
-        method: "POST",
-        body: data,
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer EAAOYsaSAfZBYBOygJffraK4UnliD3swW9Goy8NSJ1JrqE69UJRTVqSnvi4U4ZClvy3SGgW2SrOTMEmLmShMY2xzyE4VQ1fRXQdDb7bAlti3VHbJo9SYgvuQyrfOQjDV8r0SwRLJPpZCtaMlLX7LAJxnCsBsJgwUNaS5o6TDSFcJATKFx53hYk0Ax3ZCn1Lwa"
-        }
-    }
-
-    const req = https.request(options, res => {
-        res.on("data", d => { process.stdout.write(d) });
-    })
-
-    req.on("error", error => { console.error(error) })
-
-    req.write(data);
-    req.end();
-
-} */
 
 // Function to check if the text is numeric
 function isNumeric(text) {
